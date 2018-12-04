0866fcb15e7332d2183c4a9b9db5927caeb44146
Unknown <noah.steele.2021@student.lhschools.org>
2018-12-04 08:14:05 -0500

    Worked on project

--- src/LemonadeStand.java
+++ src/LemonadeStand.java
@@ -5,30 +5,26 @@ public class LemonadeStand {
         Scanner keyboard = new Scanner(System.in);
 
     double profit, supplyCost, moneyEarned, totalCost, price;
-    int supplyAmount, amountBought, amountSold;
+    int supplyAmount, amountBought, amountSold, days;
     totalCost=0;
+    moneyEarned=0;
+    supplyCost=0;
+    amountBought=0;
+    price=0;
+    amountSold=0;
 
     printALine("What is the number of supplies that are needed?");
     supplyAmount = keyboard.nextInt();
 
-    for(int x=1;x<supplyAmount+1;x++) {
-        printALine("What is the cost of supply " + x + "?");
-        supplyCost = keyboard.nextDouble();
-        printALine("How much of supply " + x + " did you buy?");
-        amountBought = keyboard.nextInt();
-        totalCost += supplyCost*amountBought;
-        }
-    System.out.println("The total cost of your supplies is $"+totalCost);
+    totalCost = getSupplyCost(supplyAmount, supplyCost, amountBought, totalCost);
 
-    printALine("\nHow much did you sell your product for?");
-    price = keyboard.nextDouble();
-    printALine("How much of your product did you sell?");
-    amountSold = keyboard.nextInt();
-    moneyEarned = price*amountSold;
-    System.out.println("You earned a total of $"+moneyEarned);
+    printALine("\nFor how many days did you sell your product?");
+    days = keyboard.nextInt();
+
+    moneyEarned=getMoneyEarned(days, price, amountSold, moneyEarned);
 
     profit = moneyEarned-totalCost;
-    System.out.println("Your total profit was $"+profit);
+    System.out.println("\nYour total profit was $"+profit);
 
 
 
@@ -36,5 +32,32 @@ public class LemonadeStand {
 
     public static void printALine(String input){
         System.out.println(input);
+    }
+
+    public static double getSupplyCost(int supplyAmount, double supplyCost, int amountBought, double totalCost){
+        Scanner keyboard = new Scanner(System.in);
+        for(int x=1;x<supplyAmount+1;x++) {
+            printALine("What is the cost of supply " + x + "?");
+            supplyCost = keyboard.nextDouble();
+            printALine("How much of supply " + x + " did you buy?");
+            amountBought = keyboard.nextInt();
+            totalCost += supplyCost*amountBought;
+        }
+        System.out.println("The total cost of your supplies is $"+totalCost);
+        return totalCost;
+    }
+
+    public static double getMoneyEarned(int days, double price, int amountSold, double moneyEarned){
+        Scanner keyboard = new Scanner(System.in);
+        for(int i=1;i<days+1;i++) {
+            printALine("\nHow much did you sell your product for on day "+i+"?");
+            price = keyboard.nextDouble();
+            printALine("How much of your product did you sell on day "+i+"?");
+            amountSold = keyboard.nextInt();
+            moneyEarned += price * amountSold;
+
+        }
+        System.out.println("You earned a total of $" + moneyEarned);
+        return moneyEarned;
     }
 }
